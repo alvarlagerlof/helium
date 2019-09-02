35f7ac327552894947fcc21e22b687e2c3a521e8
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2019-09-02 15:31:55 +0200

    Add webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+    })
+  ]
+}
